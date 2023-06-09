557d1b281c2a8c8c0df6b00269271169c4930fa7
Abdulghofar1234 <abdulghofar2367.com>
2023-06-09 11:44:22 +0700

    laravel5

--- movie-app/routes/web.php
+++ movie-app/routes/web.php
@@ -1,12 +1,9 @@
 <?php
-
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MovieController;
-use App\Http\Controllers\GenresController;
-use App\Http\Controllers\ReviewsController;
-use App\Http\Controllers\UsersController;
-
-
+use App\Http\Controllers\GenreController;
+use App\Http\Controllers\ReviewController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,20 +20,23 @@ Route::get('/', function () {
     return view('index');
 });
 
-Route::get('/movies', [MovieController::class, 'index']);
-Route::get('movies/create', [MovieController::class, 'create']);
-Route::post('movies', [MovieController::class, 'store']);
-Route::delete('/movies/{movie}', [MovieController::class, 'destroy']);
+Route::get('/index', function () {
+    return view('index');
+});
+Route::get('/', [HomeController::class, 'index']);
+// Movies
 
-Route::get('/genres', [GenresController::class, 'index']);
-Route::get('/genres/create', [GenresController::class, 'create']);
-Route::post('/genres', [GenresController::class, 'store']);
-Route::delete('/genres/{genres}', [GenresController::class, 'destroy']);
+Route::resource('/movies', MovieController::class);
 
-Route::get('/reviews', [ReviewsController::class, 'index']);
-Route::get('/reviews/create', [ReviewsController::class, 'create']);
-Route::post('reviews', [ReviewsController::class, 'store']);
-Route::delete('/reviews/{reviews}', [ReviewsController::class, 'destroy']);
+// Genres
+
+Route::get('/genres', [GenreController::class, 'index']);
+Route::get('/genres/create', [GenreController::class, 'create']);
+
+// Reviews
+Route::get('/reviews', [ReviewController::class, 'index']);
 
 
-Route::get('/users', [UsersController::class, 'index']);
+Route::get('/users', function () {
+    return view('users');
+});
